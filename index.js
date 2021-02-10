a500f698b17b3f8e754fb66f63664b13311ed6e0
Gaurav Adhikari <gaurav.adhikari1997@gmail.com>
2021-02-10 05:47:25 +0000

    added static file

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ require("./routes/postRoutes")(app);
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
 
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "static")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "static", "index.html"));
   });
 }
 
